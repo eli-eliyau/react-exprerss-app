85eb52a5025cf94ae4794e215cd045fe54cc5477
eli-Eliyahu <eli.eliyahu@280gmail.com>
2023-11-23 12:55:17 +0200

    first commit

---
+++ server/dist/app.js
@@ -0,0 +1,20 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const node_path_1 = __importDefault(require("node:path"));
+require("dotenv").config({ path: "../.env" });
+const app = (0, express_1.default)();
+const buildClient = node_path_1.default.join(__dirname, '../../client/build');
+app.use(express_1.default.json());
+app.use((0, cors_1.default)({ origin: process.env.FRONT_URL, credentials: true }));
+app.use(express_1.default.static(buildClient));
+app.get('/*', (req, res) => {
+    res.sendFile(node_path_1.default.join(__dirname, '../../client/build/index.html'), (err) => err && res.send(err));
+});
+app.listen(3001, () => {
+    console.log("3001");
+});
---
+++ server/src/app.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import path from "node:path";
+require("dotenv").config({ path: "../.env" });
+
+
+const app = express()
+const buildClient = path.join(__dirname, '../../client/build')
+
+app.use(express.json())
+app.use(cors({ origin: process.env.FRONT_URL, credentials: true }));
+app.use(express.static(buildClient))
+
+
+app.get('/*', (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, '../../client/build/index.html'), (err) =>
+    err && res.send(err))
+})
+
+app.listen(3001, () => {
+  console.log("3001");
+});
