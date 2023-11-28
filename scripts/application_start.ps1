3ffa5d4f869c6fbea8a2b52b7f8ebd6df61dc8c1
eli-Eliyahu <eli.eliyahu@280gmail.com>
2023-11-28 14:49:42 +0200

    s

--- scripts/application_start.ps1
+++ scripts/application_start.ps1
@@ -1,16 +1,31 @@
-# Give permissions to express-app directory  
-icacls "C:\project" /grant Administrator:F
+# # Give permissions to express-app directory  
+# icacls "C:\project" /grant Administrator:F
 
-# Navigate to working directory
-cd "C:\project\server" 
+# # Navigate to working directory
+# cd "C:\project\server" 
 
-# Add node and npm to path
-$env:Path += ";C:\Program Files\nodejs;"  
+# # Add node and npm to path
+# $env:Path += ";C:\Program Files\nodejs;"  
+
+# # Install node modules
+# npm install
+
+# cd dist
+
+# # Start node app in background  
+# Start-Process node app.js -RedirectStandardOutput .\app.out.log -RedirectStandardError .\app.err.log -WindowStyle Hidden
+# Give permission for everything in the express-app directory
+icacls "C:\project" /grant 'Everyone:(OI)(CI)F' /T
+
+# Navigate into the working directory where GitHub files are stored
+Set-Location -Path "C:\project"
+
+# Add npm and node to the path
+$NVM_DIR = "$env:USERPROFILE\.nvm"
+. "$NVM_DIR\nvm.ps1"  # loads nvm
 
 # Install node modules
 npm install
 
-cd dist
-
-# Start node app in background  
-Start-Process node app.js -RedirectStandardOutput .\app.out.log -RedirectStandardError .\app.err.log -WindowStyle Hidden
+# Start our node app in the background
+Start-Process node -ArgumentList "app.js" -RedirectStandardOutput "app.out.log" -RedirectStandardError "app.err.log" -NoNewWindow -Wait
