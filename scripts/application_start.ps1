f1a414d88c4731650a4cb347af528739c8ab0339
eli-Eliyahu <eli.eliyahu@280gmail.com>
2023-11-28 14:54:13 +0200

    s

--- scripts/application_start.ps1
+++ scripts/application_start.ps1
@@ -24,8 +24,12 @@ Set-Location -Path "C:\project"
 $NVM_DIR = "$env:USERPROFILE\.nvm"
 . "$NVM_DIR\nvm.ps1"  # loads nvm
 
+cd "server"
+
 # Install node modules
 npm install
+
+cd "dist"
 
 # Start our node app in the background
 Start-Process node -ArgumentList "app.js" -RedirectStandardOutput "app.out.log" -RedirectStandardError "app.err.log" -NoNewWindow -Wait
