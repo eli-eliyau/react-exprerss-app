8078b5d29a193a4e63f1eb1ce12fc57f9c45167b
eli-Eliyahu <eli.eliyahu@280gmail.com>
2023-11-29 13:59:32 +0200

    s

--- scripts/before_install.ps1
+++ scripts/before_install.ps1
@@ -1,16 +1,32 @@
-# Download and install nvm for Windows
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.ps1 -OutFile C:\Windows\system32\nvm\install-nvm.ps1
-cd C:\Windows\system32\nvm
-.\install-nvm.ps1
-# Remove-Item -Path  C:\Windows\system32\nvm\install-nvm.ps1
+# # Download and install nvm for Windows
+# Invoke-WebRequest -Uri https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.ps1 -OutFile C:\Windows\system32\nvm\install-nvm.ps1
+# cd C:\Windows\system32\nvm
+# .\install-nvm.ps1
+# # Remove-Item -Path  C:\Windows\system32\nvm\install-nvm.ps1
 
 
-# Set up nvm environment
-# . $HOME\.nvm\nvm.ps1
-cd C:\Windows\system32\nvm\.nvm\nvm.ps1
+# # Set up nvm environment
+# # . $HOME\.nvm\nvm.ps1
+# cd C:\Windows\system32\nvm\.nvm\nvm.ps1
 
-# Install Node.js
-nvm install node
+# # Install Node.js
+# nvm install node
+
+# # Create our working directory if it doesn't exist
+# $DIR = "C:\project"
+# if (Test-Path $DIR -PathType Container) {
+#     Write-Output "${DIR} exists"
+# } else {
+#     Write-Output "Creating ${DIR} directory"
+#     New-Item -ItemType Directory -Path $DIR
+# }
+
+cd C:\Windows\system32
+
+https://github.com/coreybutler/nvm-windows/releases/nvm-setup.exe
+
+
+nvm install latest
 
 # Create our working directory if it doesn't exist
 $DIR = "C:\project"
@@ -20,3 +36,5 @@ if (Test-Path $DIR -PathType Container) {
     Write-Output "Creating ${DIR} directory"
     New-Item -ItemType Directory -Path $DIR
 }
+
+
