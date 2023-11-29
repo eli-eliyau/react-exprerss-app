db09769559b0450e245441e96882161887ba5c1f
eli-Eliyahu <eli.eliyahu@280gmail.com>
2023-11-29 13:38:37 +0200

    d

--- scripts/before_install.ps1
+++ scripts/before_install.ps1
@@ -2,7 +2,7 @@
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.ps1 -OutFile C:\Windows\system32\nvm\install-nvm.ps1
 cd C:\Windows\system32\nvm
 .\install-nvm.ps1
-Remove-Item -Path  C:\Windows\system32\nvm\install-nvm.ps1
+# Remove-Item -Path  C:\Windows\system32\nvm\install-nvm.ps1
 
 
 # Set up nvm environment
