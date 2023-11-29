c62c248b43189d6434738af98a1b35f5970bab9f
eli-Eliyahu <eli.eliyahu@280gmail.com>
2023-11-29 16:26:19 +0200

    s

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsxקךןןןןןןן</code> and save to reload.
+          Edit <code>src/App.tsxקךןןן1231135ןןןן</code> and save to reload.
         </p>
         <a
           className="App-link"
