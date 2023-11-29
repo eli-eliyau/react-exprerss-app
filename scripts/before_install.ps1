68c12ba4971b8a6320374d56eddb036a8ed0df61
eli-Eliyahu <eli.eliyahu@280gmail.com>
2023-11-29 15:09:54 +0200

    s

--- scripts/before_install.ps1
+++ scripts/before_install.ps1
@@ -1,32 +1,16 @@
-# # Download and install nvm for Windows
-# Invoke-WebRequest -Uri https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.ps1 -OutFile C:\Windows\system32\nvm\install-nvm.ps1
-# cd C:\Windows\system32\nvm
-# .\install-nvm.ps1
-# # Remove-Item -Path  C:\Windows\system32\nvm\install-nvm.ps1
+# Download and install nvm for Windows
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.ps1 -OutFile C:\Windows\system32\nvm\install-nvm.ps1
+cd C:\Windows\system32\nvm
+.\install-nvm.ps1
+# Remove-Item -Path  C:\Windows\system32\nvm\install-nvm.ps1
 
 
-# # Set up nvm environment
-# # . $HOME\.nvm\nvm.ps1
-# cd C:\Windows\system32\nvm\.nvm\nvm.ps1
+# Set up nvm environment
+# . $HOME\.nvm\nvm.ps1
+cd C:\Windows\system32\nvm\.nvm\nvm.ps1
 
-# # Install Node.js
-# nvm install node
-
-# # Create our working directory if it doesn't exist
-# $DIR = "C:\project"
-# if (Test-Path $DIR -PathType Container) {
-#     Write-Output "${DIR} exists"
-# } else {
-#     Write-Output "Creating ${DIR} directory"
-#     New-Item -ItemType Directory -Path $DIR
-# }
-
-cd C:\Windows\system32
-
-https://github.com/coreybutler/nvm-windows/releases/nvm-setup.exe
-
-
-nvm install latest
+# Install Node.js
+nvm install node
 
 # Create our working directory if it doesn't exist
 $DIR = "C:\project"
@@ -36,5 +20,21 @@ if (Test-Path $DIR -PathType Container) {
     Write-Output "Creating ${DIR} directory"
     New-Item -ItemType Directory -Path $DIR
 }
+
+# cd C:\Windows\system32
+
+# https://github.com/coreybutler/nvm-windows/releases/nvm-setup.exe
+
+
+# nvm install latest
+
+# # Create our working directory if it doesn't exist
+# $DIR = "C:\project"
+# if (Test-Path $DIR -PathType Container) {
+#     Write-Output "${DIR} exists"
+# } else {
+#     Write-Output "Creating ${DIR} directory"
+#     New-Item -ItemType Directory -Path $DIR
+# }
 
 
