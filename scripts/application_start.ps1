1dbfafff405a3fdfc9d0c14ac1e498f3e5821250
eli-Eliyahu <eli.eliyahu@280gmail.com>
2023-11-29 09:50:25 +0200

    update

--- scripts/application_start.ps1
+++ scripts/application_start.ps1
@@ -15,14 +15,14 @@
 # # Start node app in background  
 # Start-Process node app.js -RedirectStandardOutput .\app.out.log -RedirectStandardError .\app.err.log -WindowStyle Hidden
 # Give permission for everything in the express-app directory
-icacls "C:\project" /grant 'Everyone:(OI)(CI)F' /T
+# icacls "C:\project" /grant 'Everyone:(OI)(CI)F' /T
 
 # Navigate into the working directory where GitHub files are stored
 Set-Location -Path "C:\project"
 
 # Add npm and node to the path
-$NVM_DIR = "$env:USERPROFILE\.nvm"
-. "$NVM_DIR\nvm.ps1"  # loads nvm
+# $NVM_DIR = "$env:USERPROFILE\.nvm"
+# . "$NVM_DIR\nvm.ps1"  # loads nvm
 
 cd "server"
 
