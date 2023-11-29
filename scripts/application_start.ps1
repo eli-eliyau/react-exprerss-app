f5b6327ce97d8daf8fd49875f58b764c49397774
eli-Eliyahu <eli.eliyahu@280gmail.com>
2023-11-29 14:26:57 +0200

    s

--- scripts/application_start.ps1
+++ scripts/application_start.ps1
@@ -1,10 +1,26 @@
 
 
+# # Navigate into the working directory where GitHub files are stored
+# Set-Location -Path "C:\project"
+
+# # Add npm and node to the path
+# $NVM_DIR = "C:\Windows\System32\nvm"
+# . "$NVM_DIR\nvm.ps1"  # loads nvm
+
+# cd "C:\project\server"
+
+# # Install node modules
+# npm install
+
+# cd "C:\project\server\dist"
+
+# # Start our node app in the background
+# Start-Process node -ArgumentList "app.js" -RedirectStandardOutput "app.out.log" -RedirectStandardError "app.err.log" -NoNewWindow -Wait
 # Navigate into the working directory where GitHub files are stored
 Set-Location -Path "C:\project"
 
-# Add npm and node to the path
-$NVM_DIR = "C:\Windows\System32\nvm"
+# Set up nvm environment
+$NVM_DIR = "C:\Users\Administrator\nvm"
 . "$NVM_DIR\nvm.ps1"  # loads nvm
 
 cd "C:\project\server"
